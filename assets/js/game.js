5fb797ed64d96948417e3af616c7f9a85cbe74b3
Clean Code Queen <shanenmsmith@gmail.com>
2021-11-10 21:18:28 -0500

    add player and enemy variables and fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,31 @@
 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var fight = function () {
+  //Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    
+  //Subtract the value of `playerAttack` from the value of `enemyHealth`and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
 
-// fight();
+  //Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health points remaining.");
+
+  //Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    
+  //Log a resulting message to the console so we know that it worked.
+    console.log("Ugh! I've been attacked by " + enemyName + ". " + "Player health is now " + playerHealth);
+};
+
+fight();
 
