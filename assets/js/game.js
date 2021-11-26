7f3bb05aad73bc33dc35d047fcf9de3720ce2687
Clean Code Queen <shanenmsmith@gmail.com>
2021-11-25 22:02:56 -0500

    updated levels to defeat multiple bots

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,31 +9,49 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
-  //Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-    
-  //Subtract the value of `playerAttack` from the value of `enemyHealth`and use that result to update the value in the `enemyHealth` variable
+  while (enemyHealth > 0) {
+    //Subtract the value of `playerAttack` from the value of `enemyHealth`and use that result to update the value in the `enemyHealth` variable
     enemyHealth = enemyHealth - playerAttack;
 
-  //Log a resulting message to the console so we know that it worked.
-    console.log(playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has " + enemyHealth + " health points remaining.");
-  
-  //Check enemy's health
-  if (enemyHealth <= 0) {
-    console.log(enemyNames[i] + " has died");
-  }
-  else {
-    console.log(enemyNames[i] + " still has " + enemyHealth + " health points left.");
-  }
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+      playerName +
+        " attacked " +
+        enemyNames[i] +
+        ". " +
+        enemyNames[i] +
+        " now has " +
+        enemyHealth +
+        " health points remaining."
+    );
 
-  //Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    //Check enemy's health
+    if (enemyHealth <= 0) {
+      console.log(enemyNames[i] + " has died");
+    } else {
+      console.log(
+        enemyNames[i] + " still has " + enemyHealth + " health points left."
+      );
+    }
+
+    //Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
-    
-  //Log a resulting message to the console so we know that it worked.
-    console.log("Ugh! I've been attacked by " + enemyNames[i] + ". " + "Player health is now " + playerHealth);
+
+    //Log a resulting message to the console so we know that it worked.
+    console.log(
+      "Ugh! I've been attacked by " +
+        enemyNames[i] +
+        ". " +
+        "Player health is now " +
+        playerHealth
+    );
+  }
+  
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
   fight(enemyNames[i]);
 }
 
