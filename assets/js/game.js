4f709f9c5e454f0e24db54cc3875d35a33a14ec5
Clean Code Queen <shanenmsmith@gmail.com>
2021-11-25 21:38:13 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,15 +2,13 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-var fight = function () {
+var fight = function (enemyName) {
   //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -18,14 +16,34 @@ var fight = function () {
     enemyHealth = enemyHealth - playerAttack;
 
   //Log a resulting message to the console so we know that it worked.
-    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health points remaining.");
+    console.log(playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has " + enemyHealth + " health points remaining.");
+  
+  //Check enemy's health
+  if (enemyHealth <= 0) {
+    console.log(enemyNames[i] + " has died");
+  }
+  else {
+    console.log(enemyNames[i] + " still has " + enemyHealth + " health points left.");
+  }
 
   //Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
     
   //Log a resulting message to the console so we know that it worked.
-    console.log("Ugh! I've been attacked by " + enemyName + ". " + "Player health is now " + playerHealth);
+    console.log("Ugh! I've been attacked by " + enemyNames[i] + ". " + "Player health is now " + playerHealth);
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
+
+
+//Game States
+//"WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy-robots
+//  * Defeat Each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+//Pick up at 3.2.5 Fight Enemy Robot Combatants Using for Loops 11/25/2021
 
